8c194a50346225009a6506a10b26b0350980f5ed
Bảo Nguyễn Cao Quốc <21520611@ms.uit.edu.vn>
2023-05-10 17:08:29 +0700

    copy base files from Collisions

---
+++ SuperMarioBros3Project/Brick.cpp
@@ -0,0 +1,20 @@
+#include "Brick.h"
+
+void CBrick::Render()
+{
+	CAnimations* animations = CAnimations::GetInstance();
+	animations->Get(ID_ANI_BRICK)->Render(x, y);
+}
+
+void CBrick::GetBoundingBox(float &l, float &t, float &r, float &b)
+{
+	l = x - BRICK_BBOX_WIDTH/2;
+	t = y - BRICK_BBOX_HEIGHT/2;
+	r = l + BRICK_BBOX_WIDTH;
+	b = t + BRICK_BBOX_HEIGHT;
+}
+void CGoldBrick::Render()
+{
+	CAnimations* animations = CAnimations::GetInstance();
+	animations->Get(ID_ANI_GOLD_BRICK)->Render(x, y);
+}
---
+++ SuperMarioBros3Project/Brick.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "GameObject.h"
+#include "Animation.h"
+#include "Animations.h"
+
+#define ID_ANI_BRICK 10000
+#define ID_ANI_GOLD_BRICK 10100
+#define BRICK_WIDTH 16
+#define BRICK_BBOX_WIDTH 16
+#define BRICK_BBOX_HEIGHT 16
+
+class CBrick : public CGameObject {
+public:
+	CBrick(float x, float y) : CGameObject(x, y) {}
+	void Render();
+	void Update(DWORD dt) {}
+	void GetBoundingBox(float& l, float& t, float& r, float& b);
+};
+class CGoldBrick : public CBrick {
+public:
+	CGoldBrick(float x, float y) : CBrick(x,y) {}
+	void Render();
+	void Update(DWORD dt) {}
+};
